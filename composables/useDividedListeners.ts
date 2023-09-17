f88f640cd1c579cf3a062b397c9b98aee45491b4
Brix-D <Shlyakhta4@gmail.com>
2023-09-17 17:04:41 +0400

    fix(b input): try to fix b input for vue 3

--- composables/useDividedListeners.ts
+++ composables/useDividedListeners.ts
@@ -1,7 +1,7 @@
 import { computed, useAttrs, isVue2, getCurrentInstance } from 'vue-demi';
 
 export function useDividedListeners() {
-    let $listeners = {};
+    let $listeners: Record<string, any> | unknown = {};
     if (isVue2) {
         const instance = getCurrentInstance();
         // @ts-nocheck
@@ -18,7 +18,7 @@ export function useDividedListeners() {
     }
 
     const listeners = computed(() => {
-        return Object.keys($listeners).reduce<{
+        return Object.keys($listeners as Record<string, any>).reduce<{
             prepend: Record<string, any>;
             append: Record<string, any>;
             content: Record<string, any>;
@@ -26,15 +26,15 @@ export function useDividedListeners() {
             (acc, key) => {
                 if (key.endsWith(':prepend')) {
                     const newKey = key.replace(/:prepend$/, '');
-                    acc['prepend'][newKey] = $listeners[key];
+                    acc['prepend'][newKey] = ($listeners as Record<string, any>)[key];
                     return acc;
                 }
                 if (key.endsWith(':append')) {
                     const newKey = key.replace(/:append$/, '');
-                    acc['append'][newKey] = $listeners[key];
+                    acc['append'][newKey] = ($listeners as Record<string, any>)[key];
                     return acc;
                 }
-                acc['content'][key] = $listeners[key];
+                acc['content'][key] = ($listeners as Record<string, any>)[key];
                 return acc;
             },
             { prepend: {}, append: {}, content: {} }
